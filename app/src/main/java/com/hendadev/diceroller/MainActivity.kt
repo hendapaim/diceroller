8a3c1da310e4ecb60c5e6d3f7cd47ef03ed4c133
hendapaim <hendapaim278@gmail.com>
2022-08-22 00:45:24 +0100

    Button funcionando

--- app/src/main/java/com/hendadev/diceroller/MainActivity.kt
+++ app/src/main/java/com/hendadev/diceroller/MainActivity.kt
@@ -2,10 +2,25 @@ package com.hendadev.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        val numberText: TextView = findViewById(R.id.number_text)
+        rollButton.setOnClickListener {
+            numberText.text = numberRandom()
+            //Toast.makeText(this, "Botão clicado", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun numberRandom(): String {
+        val number = arrayOf(1,2,3,4,5,6)
+        return number[Random().nextInt(6)].toString()
     }
 }
