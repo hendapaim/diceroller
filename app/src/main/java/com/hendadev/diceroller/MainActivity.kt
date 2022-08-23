a6459651100748602b6a10d001eeb481b05eee5f
hendapaim <hendapaim278@gmail.com>
2022-08-23 11:34:29 +0100

    Dice Image adicionado

--- app/src/main/java/com/hendadev/diceroller/MainActivity.kt
+++ app/src/main/java/com/hendadev/diceroller/MainActivity.kt
@@ -3,25 +3,39 @@ package com.hendadev.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+    lateinit var rollButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.roll_button)
-        val numberText: TextView = findViewById(R.id.number_text)
+        // Logic for my application
+        diceImage = findViewById(R.id.dice_image)
+        rollButton = findViewById(R.id.roll_button)
+
         rollButton.setOnClickListener {
-            numberText.text = numberRandom()
+            rollDice()
             //Toast.makeText(this, "Botão clicado", Toast.LENGTH_SHORT).show()
             //Snackbar.make(this, R.id.number_text, Snackbar.LENGTH_SHORT).show()
         }
+
     }
 
-    private fun numberRandom(): String {
-        val number = arrayOf(1, 2, 3, 4, 5, 6)
-        return number[Random().nextInt(6)].toString()
+    private fun rollDice() {
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
